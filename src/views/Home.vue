dff909b285cd4e94bec7edbee61ce20f64f715ab
Anshari16 <m.luthfianshari89@gmail.com>
2022-01-27 15:38:46 +0800

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroSection />
+    <BanerShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+    
+  </div>
+</template>
+
+<script>
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+
+import HeroSection from '@/components/HeroSection.vue';
+import BanerShayna from '@/components/BanerShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/PartnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    HeaderShayna,
+    HeroSection,
+    BanerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }}
+</script>
